96e9559b72c232e2df5f3b7f5e13b9d264873d84
Jan Baudisch <dev@baudisch.xyz>
2019-11-13 10:41:06 +0000

    update ci

--- .travis/install-cargo-web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/koute/cargo-web/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/' | cut -c 1-)
-wget https://github.com/koute/cargo-web/releases/download/${RELEASE}/cargo-web-x86_64-unknown-linux-gnu.gz
-gunzip cargo-web-x86_64-unknown-linux-gnu.gz
-chmod +x cargo-web-x86_64-unknown-linux-gnu
-sudo mv cargo-web-x86_64-unknown-linux-gnu /usr/bin/cargo-web
